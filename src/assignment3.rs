085c1c81c790075258b4763f075fd7166834f2e5
SecondarySkyler <skylerhunter6@gmail.com>
2024-09-28 10:31:32 +0200

    added exercises

--- src/assignment3.rs
+++ src/assignment3.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+use core::{fmt, str};
 use std::collections::HashMap;
 // Exercise 1: Given a number determine whether it is valid per the Luhn formula
 pub fn is_it_luhn(s: String) -> bool {
@@ -142,6 +143,208 @@ impl VendingMachine {
     }
 }
 
+// Exercise 5: Implement two tuple structs named Date and Hour
+#[derive(Debug)]
+struct Date {
+    day: u8,
+    month: u8,
+    year: u16
+}
+
+#[derive(Debug)]
+struct Hour {
+    hour: u8,
+    minute: u8,
+}
+
+#[derive(Debug)]
+struct BoxShipping {
+    name: String,
+    barcode: String,
+    shipment_date: Date,
+    shipment_hour: Hour
+}
+
+impl fmt::Display for Date {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02}/{:02}/{:04}", self.day, self.month, self.year)
+    }
+}
+
+impl fmt::Display for Hour {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hour, self.minute)
+    }
+}
+
+impl fmt::Display for BoxShipping {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Name: {}\nBarcode: {}\nShipment Date: {}\nShipment Hour: {}", self.name, self.barcode, self.shipment_date, self.shipment_hour)
+    }
+}
+
+// Exercise 6: Create BUP's library system. It should be able to store books, articles and magazines.
+struct Book {
+    name: String,
+    code: u32,
+    year_of_publication: u16,
+    author: String,
+    publisher: String
+}
+
+struct Article {
+    name: String,
+    code: u32,
+    year_of_publication: u16,
+    orchid: String
+}
+
+struct Magazine {
+    name: String,
+    code: u32,
+    year_of_publication: u16,
+    number: u32,
+    month: u8
+}
+
+struct Bup {
+    books: Vec<Book>,
+    articles: Vec<Article>,
+    magazines: Vec<Magazine>
+}
+
+// maybe there is a better way to do this, avoiding three vectors
+// also I won't implement the Display trait :§
+impl Bup {
+    fn new() -> Bup {
+        Bup {
+            books: Vec::new(),
+            articles: Vec::new(),
+            magazines: Vec::new()
+        }
+    }
+
+    fn add_book(&mut self, book: Book) {
+        self.books.push(book);
+    }
+
+    fn add_article(&mut self, article: Article) {
+        self.articles.push(article);
+    }
+
+    fn add_magazine(&mut self, magazine: Magazine) {
+        self.magazines.push(magazine);
+    }
+
+    
+}
+
+// Exercise 7: Create a module called Point that inside has a struct Point with the fields x: f32 , y: f32
+pub mod point {
+    pub struct Point {
+        pub x: f32,
+        pub y: f32
+    }
+
+    impl Point {
+        pub fn new(x: f32, y: f32) -> Point {
+            Point {
+                x,
+                y
+            }
+        }
+
+        pub fn distance(&self, other: &Point) -> f32 {
+            ((self.x - other.x).powi(2) + (self.y - other.y).powi(2)).sqrt()
+        }
+    }
+}
+
+pub mod line {
+    use std::f32::consts::E;
+
+    use super::point::Point;
+
+    pub struct Line {
+        pub start: Point,
+        pub end: Point,
+        pub m: f32,
+        pub q: f32
+    }
+
+    impl Line {
+        pub fn new(start: Point, end: Point) -> Line {
+            let m = (end.y - start.y) / (end.x - start.x);
+            let q = start.y - m * start.x;
+            Line {
+                start,
+                end,
+                m,
+                q
+            }
+        }
+
+        pub fn contains(&self, point: &Point) -> Result<(), String> {
+            if (self.m * point.x + self.q) == point.y {
+                Ok(())
+            } else {
+                Err("Point is not in the line".to_string())
+            }
+        }
+        
+    }
+}
+
+pub mod sentence {
+    pub struct Sentence {
+        pub words: Vec<String>
+    }
+
+    impl Sentece {
+        pub fn new_default() -> Sentence {
+            Sentence {
+                words: Vec::new()
+            }
+        }
+
+        pub fn new(s: &str) -> Sentence {
+            Sentence {
+                words: s.split_whitespace().map(|s| s.to_string()).collect()
+            }
+        }
+    }
+}
+
+mod magicmodule {
+    use std::collections::HashMap;
+
+    use super::sentence::Sentence;
+
+    pub fn magic_sentence(hm: &mut HashMap<i32, Sentence>, i: i32, j: i32) -> Result<Sentence, &str> {
+        let o1 = hm.get(&i);
+        let o2 = hm.get(&j);
+
+        if o1.is_none() && o2.is_none() {
+            return Err("Both sentences are not in the hashmap");
+        }
+
+        let s1 = o1.unwrap().words;
+        let s2 = o2.unwrap().words;
+
+        Ok(Sentence {
+            words: s1.iter().zip(s2.iter()).filter_map(|(w1, w2)| {
+                if w1 == w2 {
+                    Some(w1.clone())
+                } else {
+                    None
+                }
+            }).collect()
+        })
+
+    
+    }
+}
+
 #[cfg(test)]
 mod test3 {
     use super::*;
@@ -149,5 +352,15 @@ mod test3 {
     #[test]
     fn test_is_it_luhn() {
         assert_eq!(is_it_luhn("4539 3195 0343 6467".to_string()), true);
+    }
+
+    #[test]
+    fn test_contains() {
+        let p1 = point::Point::new(1.0, 1.0);
+        let p2 = point::Point::new(2.0, 2.0);
+        let p3 = point::Point::new(3.0, 3.0);
+        let l = line::Line::new(p1, p2);
+        assert_eq!(l.contains(&p3), Ok(()));
+
     }
 }
